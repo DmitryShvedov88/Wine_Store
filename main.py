c9305ef824b7373ee06e9e79aa285b0f7c186eed
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-06-06 20:51:09 +0300

    made steps 2-9

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import datetime
+from incline_numerals import incline
+from datetime import date
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+opend_time = datetime.datetime(year=1920, month=12, day=24, hour=11)
+today = date.today()
+age = today.year - opend_time.year - ((today.month, today.day) < (opend_time.month, opend_time.day))
+inclined = incline(age)
+rendered_page = template.render(
+    text1="Проверенно временем",
+    text2=f"Уже {age} {inclined} с Вами"
+    )
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
