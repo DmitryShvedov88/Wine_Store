359a005a234889539440bc37518263b638138a85
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-06-14 20:35:38 +0300

    end lesson

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import datetime
+import collections
+import pandas
 from incline_numerals import incline
 from datetime import date
+from pprint import pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
-
 template = env.get_template('template.html')
 opend_time = datetime.datetime(year=1920, month=12, day=24, hour=11)
 today = date.today()
 age = today.year - opend_time.year - ((today.month, today.day) < (opend_time.month, opend_time.day))
 inclined = incline(age)
+load_wines = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='records')
+wines_many = collections.defaultdict(list)
+[wines_many[wine["Категория"]].append(wine) for wine in load_wines]
 rendered_page = template.render(
     text1="Проверенно временем",
-    text2=f"Уже {age} {inclined} с Вами"
+    text2=f"Уже {age} {inclined} с Вами",
+    wines=wines_many
     )
 
 with open('index.html', 'w', encoding="utf8") as file:
