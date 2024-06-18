561f54a8e6d6f2e6f48b0435c8ca6df6353715f8
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-06-18 22:30:13 +0300

    made another dev

--- main.py
+++ main.py
@@ -1,40 +1,77 @@
 """Module providing a function counting age."""
+import os
 import datetime
 from datetime import date
 import collections
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import argparse
 import pandas
 from incline_numerals import incline
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
-opend_time = datetime.datetime(year=1920, month=12, day=24, hour=11)
-today = date.today()
-age = (today.year - opend_time.year
-       - ((today.month, today.day) < (opend_time.month, opend_time.day))
-       )
-inclined = incline(age)
-load_wines = pandas.read_excel(
-    'wine3.xlsx',
-    na_values=['N/A', 'NA'],
-    keep_default_na=False).to_dict(orient='records')
-wines = collections.defaultdict(list)
-for wine in load_wines:
-    wines[wine["Категория"]].append(wine)
+def get_path(wines_path):
+    parser = argparse.ArgumentParser(
+        description='Программа позволяет разворачивать сайт по продаже винного магазина, с перечнем товаров из Эксель файла'
+        )
+    parser.add_argument(
+        '--product_path',
+        help='Введите --product_path путь к файлу',
+        type=str,
+        default=wines_path
+        )
+    product_range = parser.parse_args()
+    products = product_range.product_path
+    return products
 
-rendered_page = template.render(
-    top_heading="Проверенно временем",
-    bottom_header=f"Уже {age} {inclined} с Вами",
-    wines=wines
+
+def get_products(file_path):
+    df_wines = load_wines(file_path)
+    wines = collections.defaultdict(list)
+    for wine in df_wines:
+        wines[wine["Категория"]].append(wine)
+    return wines
+
+
+def load_wines(products):
+    """Take a DF from Excel"""
+    df = pandas.read_excel(
+        products,
+        na_values=['N/A', 'NA'],
+        keep_default_na=False).to_dict(orient='records')
+    return df
+
+
+def count_age():
+    opend_time = datetime.datetime(year=1920, month=12, day=24, hour=11)
+    today = date.today()
+    age = (today.year - opend_time.year
+        - ((today.month, today.day) < (opend_time.month, opend_time.day))
+        )   
+    return age
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
+    template = env.get_template('template.html')
+    wines_path = os.getenv('FILE')
+    file_path = get_path(wines_path)
+    wines = get_products(file_path)
+    age = count_age()
+    inclined = incline(age)
+    rendered_page = template.render(
+        top_heading="Проверенно временем",
+        bottom_header=f"Уже {age} {inclined} с Вами",
+        wines=wines
+        )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
